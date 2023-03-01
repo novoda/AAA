f3331f7878346b8113c78ef1359b8548fb28c601
Carl-Gustaf Harroch <carl@novoda.com>
2023-03-01 17:25:59 +0000

    init

---
+++ Hello/index.js
@@ -0,0 +1,9 @@
+﻿module.exports = async function (context) {
+    //Thread.sleep(100000)
+    await sleep(10000)
+    return `Hello ${context.bindings.name}!`;
+};
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
